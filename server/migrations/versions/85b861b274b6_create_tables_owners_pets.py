493b4ad0c1973911466cd1bfd0193fd1c4378fb0
Wesley Peters <wesleypeters@gmail.com>
2023-07-24 10:57:21 -0600

    complete lesson

---
+++ server/migrations/versions/85b861b274b6_create_tables_owners_pets.py
@@ -0,0 +1,24 @@
+"""Create tables owners, pets
+
+Revision ID: 85b861b274b6
+Revises: 
+Create Date: 2023-07-24 10:48:54.631112
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '85b861b274b6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
